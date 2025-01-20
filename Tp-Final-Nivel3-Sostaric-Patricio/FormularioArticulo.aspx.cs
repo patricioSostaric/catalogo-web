b070fd930aec4a6b526ac1a0c38dbfe5016bfba8
Patricio sostaric <patricio.jp.01sostaric@gmail.com>
2025-01-20 13:10:10 -0300

    Creacion Formulario Articulo

---
+++ Tp-Final-Nivel3-Sostaric-Patricio/FormularioArticulo.aspx.cs
@@ -0,0 +1,138 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tp_Final_Nivel3_Sostaric_Patricio
+{
+
+    public partial class FormularioArticulo : System.Web.UI.Page
+    {
+        public bool ConfirmaEliminacion { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtId.Enabled = false;
+            ConfirmaEliminacion = false;
+            try
+            {
+                //configuración inicial de la pantalla.
+                if (!IsPostBack)
+                {
+                    CategoriaNegocio negocio = new CategoriaNegocio();
+                    List<Categoria> lista = negocio.listar();
+
+                    MarcaNegocio negocio1 = new MarcaNegocio();
+                    List<Marca> lista1 = negocio1.listar();
+
+                    ddlCategoria.DataSource = lista;
+                    ddlCategoria.DataValueField = "Id";
+                    ddlCategoria.DataTextField = "Descripcion";
+                    ddlCategoria.DataBind();
+
+                    ddlMarca.DataSource = lista1;
+                    ddlMarca.DataValueField = "Id";
+                    ddlMarca.DataTextField = "Descripcion";
+                    ddlMarca.DataBind();
+
+                }
+                //configuración si estamos modificando.
+                string id = Request.QueryString["id"] != null ? Request.QueryString["id"].ToString() : "";
+                if (id != "" && !IsPostBack)
+                {
+                    ArticuloNegocio negocio = new ArticuloNegocio();
+                    //List<Articulo> lista = negocio.listar(id);
+                    //Articulo seleccionado = lista[0];
+                    Articulo seleccionado = (negocio.listar(id))[0];
+
+                    //guardo Articulo seleccionado en session
+                    Session.Add("ArticuloSeleccionado", seleccionado);
+
+                    //pre cargar todos los campos...
+                    txtId.Text = id;
+                    txtNombre.Text = seleccionado.Nombre;
+                    txtDescripcion.Text = seleccionado.Descripcion;
+                    txtImagenUrl.Text = seleccionado.ImagenUrl;
+                    txtCodigo.Text = seleccionado.Codigo.ToString();
+
+
+                    ddlCategoria.SelectedValue = seleccionado.TipoCategoria.Id.ToString();
+                    ddlMarca.SelectedValue = seleccionado.TipoMarca.Id.ToString();
+                    txtImagenUrl_TextChanged(sender, e);
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex);
+                Response.Redirect("Error.aspx");
+            }
+        }
+
+        protected void btnAceptar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Articulo nuevo = new Articulo();
+                ArticuloNegocio negocio = new ArticuloNegocio();
+
+                nuevo.Codigo = txtCodigo.Text;
+                nuevo.Nombre = txtNombre.Text;
+                nuevo.Descripcion = txtDescripcion.Text;
+                nuevo.ImagenUrl = txtImagenUrl.Text;
+
+                nuevo.TipoCategoria = new Categoria();
+                nuevo.TipoCategoria.Id = int.Parse(ddlCategoria.SelectedValue);
+                nuevo.TipoMarca = new Marca();
+                nuevo.TipoMarca.Id = int.Parse(ddlMarca.SelectedValue);
+
+                if (Request.QueryString["id"] != null)
+                {
+                    nuevo.Id = int.Parse(txtId.Text);
+                    negocio.modificarConSP(nuevo);
+                }
+                else
+                    negocio.agregarConSP(nuevo);
+
+
+                Response.Redirect("ArticuloLista.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex.ToString());
+                Response.Redirect("Error.aspx");
+            }
+        }
+
+        protected void txtImagenUrl_TextChanged(object sender, EventArgs e)
+        {
+            imgArticulo.ImageUrl = txtImagenUrl.Text;
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            ConfirmaEliminacion = true;
+        }
+
+        protected void btnConfirmaEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkConfirmaEliminacion.Checked)
+                {
+                    ArticuloNegocio negocio = new ArticuloNegocio();
+                    negocio.eliminar(int.Parse(txtId.Text));
+                    Response.Redirect("ArticuloLista.aspx");
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex.ToString());
+                Response.Redirect("Error.aspx");
+            }
+
+        }
+    }
+}
